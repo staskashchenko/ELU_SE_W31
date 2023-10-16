8f0bc916395664b5c3bdaa5ec6de6ea61bb544cd
Stanislav Kashchenko <stanislav.kashchenko@microavia.com>
2023-10-16 18:32:07 +0400

    Updated function and variable naming to comply with PEP8 standards. Added docstrings to functions. Resolved the type error by changing 'Item C' price from string to float. Adjusted display_total to correctly format float values.

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,22 +1,22 @@
-def CalculateTotal(cart):
+def calculate_total(cart):
+    """Calculates the total price of items in the cart."""
     total = 0
-    total_items = len(cart)
     for item in cart:
         total += item['price']
     return total
 
-def display_total(Total):
-    print("Total price: " + Total)
+def display_total(total):
+    """Displays the total price."""
+    print(f"Total price: ${total:.2f}")
 
-CART = [
+CART_ITEMS = [
     {'name': 'Item A', 'price': 10.99},
     {'name': 'Item B', 'price': 5.99},
-    {'name': 'Item C', 'price': '8.49'}
+    {'name': 'Item C', 'price': 8.49}
 ]
 
-for item in CART:
-    print(f"Item: {item['name']} - Price: ${item['price']}")
+for item in CART_ITEMS:
+    print(f"Item: {item['name']} - Price: ${item['price']:.2f}")
 
-shopping_cart_total = CalculateTotal(CART)
+shopping_cart_total = calculate_total(CART_ITEMS)
 display_total(shopping_cart_total)
-
